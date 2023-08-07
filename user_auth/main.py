aa262d9fb2a42ec8a1c18932f60ce865557fb623
Oitz <oitz@oitz.vool>
2023-08-07 22:07:13 +0300

    Create structures, primary settings

--- user_auth/main.py
+++ user_auth/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-from .api import router
+from .api.api import router
+from user_auth.core.config import settings
 
-app = FastAPI()
+
+app = FastAPI(title=settings.app_title)
 
 app.include_router(router)
